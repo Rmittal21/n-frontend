3e9d24dc845a32621fcfb70667f1a6f0f6ba2913
Nicky Rutten <nicky@groupvandamme.eu>
2015-11-09 11:03:19 +0100

    changed js for slick

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -16,8 +16,8 @@ $(document).ready(function() {
 	
     $(".gallery").slick({
     	infinite: true,
-  		slidesToShow: 1,
-  		slidesToScroll: 1,
+  		slidesToShow: 3,
+  		slidesToScroll: 3,
   		arrows: false
   	});
 
